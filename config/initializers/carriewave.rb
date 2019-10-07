bd2e8817cac1b3328b13f172c0d57a29cb48bf3e
kazkosa <kazu_kosa@icloud.com>
2019-10-07 16:43:01 +0900

    Update credential.yml.enc

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -17,11 +17,15 @@ CarrierWave.configure do |config|
       # aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
 
       #aws_access_key_id: Rails.application.credentials.aws_access_key_id
-      aws_access_key_id: Rails.application.credentials.aws[:secret_key_base],
+      # aws_access_key_id: eval(Rails.application.credentials.aws[:secret_key_base]),
+      
       # aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
       # aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      aws_secret_access_key: Rails.application.credentials.aws[:aws_access_key_id],
-
+      # aws_secret_access_key: eval(Rails.application.credentials.aws[:aws_access_key_id]),
+      aws_access_key_id: eval(Rails.application.credentials.aws[:aws_access_key_id]),
+      aws_secret_access_key: eval(Rails.application.credentials.aws[:aws_secret_access_key]),      
+      secret_key_base: eval(Rails.application.credentials.secret_key_base),
+      
       region: 'ap-northeast-1' #'AWSで設定した地域（おそらく'ap-northeast-1') '
     }
     config.fog_directory  = 'backet191006' #S3のバケット名'
