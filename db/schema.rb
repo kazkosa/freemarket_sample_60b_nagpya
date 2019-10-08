92a0775af023850d49c6325f292e996df903f969
kazkosa <kazu_kosa@icloud.com>
2019-10-08 10:39:56 +0900

    Create address model and addresses migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_010114) do
+ActiveRecord::Schema.define(version: 2019_10_08_011923) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "prefectures", null: false
+    t.string "municipalities", null: false
+    t.string "block_number", null: false
+    t.string "building"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "trials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image"
@@ -38,4 +50,5 @@ ActiveRecord::Schema.define(version: 2019_10_08_010114) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
 end
