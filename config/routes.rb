75dc4e16aa3bd65711185ceab841f16c7f1ff5f2
kazkosa <kazu_kosa@icloud.com>
2019-10-09 18:20:03 +0900

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
+  resources :products
+  resources :users do
+    collection do
+      get :card
+    end
+  end
   resources :trials, only: [:index,:create,:new]
 end
