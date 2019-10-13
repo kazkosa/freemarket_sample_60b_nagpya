8eabea427e87cfa0214c4b617b7f7c4c3e6250ee
kazkosa <kazu_kosa@icloud.com>
2019-10-13 21:22:37 +0900

    Create show action in product controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
-  resources :products ,   only: :index do
+  resources :products , only: [:index, :show] do
     collection do
       get :buy
     end
