81c6cbd445f8451e6eede60c318d0c40fa98ede3
Yuri Isiguro <yuriisiguro@YurinoMacBook-Air.local>
2019-10-13 21:09:39 +0900

    create_sellpage1

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
-  resources :products ,   only: :index
-  resources :users do
+  resources :products ,   only: :index do
+    collection do
+      get :sell
+    end
+  end
+   resources :users do
     collection do
       get :plofile
       get :identification
