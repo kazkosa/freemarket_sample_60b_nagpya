d1fe0859de59e976dfe0a8a96bf5fe32a9eda836
Yuri Isiguro <yuriisiguro@YurinoMacBook-Air.local>
2019-10-24 18:51:40 +0900

    修正

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -2,13 +2,11 @@ $(function(){
   $("#modal-open-btn").on("click",function(e){
     e.preventDefault();
     $("#overlay").fadeIn();
-    console.log("good night");
   })
 });
 $(function(){
   $("#modal-close-btn").on("click",function(e){
     e.preventDefault();
     $('#overlay').fadeOut();
-    console.log("hello");
   })
 });
