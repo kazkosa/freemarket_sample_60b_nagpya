a9914ff7b53c741f20a58fa726cbdb847e6284fa
kazkosa <kazu_kosa@icloud.com>
2019-10-28 14:13:42 +0900

    change setting to retry deploy

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,4 @@ module FreemarketSample60bNagpya
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     
   end
-  ActiveRecord::Base.protected_environments = %w(staging)
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -40,23 +40,7 @@ set :linked_files, %w{ config/credentials.yml.enc }
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-  desc 'upload credentials.yml.enc'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
-end
-
-namespace :deploy do
+  
 
   task :db_reset do
     on roles(:app) do
@@ -78,4 +62,18 @@ namespace :deploy do
     end
   end
 
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+  desc 'upload credentials.yml.enc'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
